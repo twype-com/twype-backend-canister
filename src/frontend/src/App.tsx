cd7f4148646ad56086dfe76148af100684ea14e2
Sergey Eroshenkov <itechmeat@gmail.com>
2023-12-15 21:05:14 +0100

    Add favicons

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/safari-pinned-tab.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,12 +10,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <img src={viteLogo} className="logo" alt="Vite logo" width="100px" />
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
