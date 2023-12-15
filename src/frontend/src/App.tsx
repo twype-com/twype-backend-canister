1188fcd8a6b1e25ee665328e5c7a154c4f88d8f2
Sergey Eroshenkov <itechmeat@gmail.com>
2023-12-15 17:31:19 +0100

    Add logs

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   console.log('🚀 ~ App ~ count:', count)
+  console.log('🚀 ~ process.env:', process.env)
+  console.log('🚀 ~ process.env.DFX_NETWORK:', process.env.DFX_NETWORK)
 
   return (
     <>
@@ -19,16 +21,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
