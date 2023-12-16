7c5a64fad5a65ae5be2f586299279789ff72fe8b
Sergey Eroshenkov <itechmeat@gmail.com>
2023-12-16 14:53:44 +0100

    Twiks

--- src/frontend/src/components/Logo/Logo.tsx
+++ src/frontend/src/components/Logo/Logo.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react'
-// import { Righteous } from 'next/font/google'
 import cn from 'classnames'
 import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
-
-// const righteous = Righteous({ weight: '400', subsets: ['latin'] })
-// TODO: Add font
 
 type LogoProps = {
   className?: string
@@ -13,7 +9,6 @@ type LogoProps = {
 
 export const Logo: FC<LogoProps> = ({ className }) => {
   return (
-    // <Link to="/" className={cn(styles.brand, righteous.className, className)}>
     <Link to="/" className={cn(styles.brand, className)}>
       <div className={styles.logo}>Twype</div>
     </Link>
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
 import mkcert from 'vite-plugin-mkcert'
+import webfontDownload from 'vite-plugin-webfont-dl'
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +19,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
     }),
+    webfontDownload(['https://fonts.googleapis.com/css2?family=Righteous&display=swap']),
+    analyzer(),
   ],
   resolve: {
     alias: [
